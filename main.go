c9f3112206d6044008167b231116a8b7d41d6439
Nick Gregory <github@openenterprise.co.uk>
2018-08-14 11:17:22 +0000

    interim commit

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
     "flag"
     "fmt"
     "time"
+    "sync"
+    "runtime"
     "strconv"
     "strings"
     "syscall"
     "github.com/pkg/errors"
-    "github.com/elastic/go-libaudit"
     "github.com/golang/glog"
+    "github.com/elastic/go-libaudit"
+    "github.com/elastic/go-libaudit/aucoalesce"
     "github.com/elastic/go-libaudit/auparse"
 )
 
@@ -31,6 +34,16 @@ const (
 )
 
 type backpressureStrategy uint8
+
+type AuditSet struct {
+        config     Config
+        client     *libaudit.AuditClient
+        kernelLost struct {
+                enabled bool
+                counter uint32
+        }
+        backpressureStrategy backpressureStrategy
+}
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n", )
@@ -60,6 +73,12 @@ func main() {
 
     defer client.Close()
 
+    as := &AuditSet{
+        client: client,
+        config: config,
+        backpressureStrategy: getBackpressureStrategy(config.BackpressureStrategy),
+    }
+
     status, err := client.GetStatus()
     if err != nil {
         glog.Error("failed to get audit status")
@@ -68,26 +87,179 @@ func main() {
 
     glog.Infof("received audit status=%+v", status)
 
-    receive(client)
+    done := make(chan struct{})
+    defer close(done)
+
+    out, err := receiveEvents(as, done)
+    if err != nil {
+        glog.Error("Failure receiving audit events")
+        return
+    }
+
+    if as.kernelLost.enabled {
+
+        client, err := libaudit.NewAuditClient(nil)
+        if err != nil {
+            glog.Errorln("Failure creating audit monitoring client", "error", err)
+        }
+        go func() {
+            defer client.Close()
+            timer := time.NewTicker(lostEventsUpdateInterval)
+            defer timer.Stop()
+            for {
+                select {
+                    case <-done:
+                        return
+                    case <-timer.C:
+                        if status, err := client.GetStatus(); err == nil {
+                            updateKernelLostMetric(as, status.Lost)
+                        } else {
+                            glog.Error("get status request failed:", err)
+                        }
+                }
+            }
+        }()
+    }
+
+    // Spawn the stream buffer consumers
+    numConsumers := as.config.StreamBufferConsumers
+
+    // By default (stream_buffer_consumers=0) use as many consumers as local CPUs
+    // with a max of `maxDefaultStreamBufferConsumers`
+    if numConsumers == 0 {
+        if numConsumers = runtime.GOMAXPROCS(-1); numConsumers > maxDefaultStreamBufferConsumers {
+            numConsumers = maxDefaultStreamBufferConsumers
+        }
+    }
+    var wg sync.WaitGroup
+    wg.Add(numConsumers)
+
+    for i := 0; i < numConsumers; i++ {
+        go func() {
+            defer wg.Done()
+            for {
+                select {
+                    case <-done:
+                        return
+                    case msgs := <-out:
+                        buildEvent(msgs, as.config)
+                }
+            }
+        }()
+    }
+    wg.Wait()
 
     return
 }
 
-func receive(r *libaudit.AuditClient) error {
-	for {
-		rawEvent, err := r.Receive(false)
-		if err != nil {
-			return errors.Wrap(err, "receive failed")
-		}
+func buildEvent(msgs []*auparse.AuditMessage, config Config) {
 
-		// Messages from 1300-2999 are valid audit messages.
-		if rawEvent.Type < auparse.AUDIT_USER_AUTH ||
-			rawEvent.Type > auparse.AUDIT_LAST_USER_MSG2 {
-			continue
-		}
+    auditEvent, err := aucoalesce.CoalesceMessages(msgs)
 
-		fmt.Printf("type=%v msg=%v\n", rawEvent.Type, string(rawEvent.Data))
-	}
+        if err != nil {
+                glog.Error("Failure to CoalesceMessages events")
+                return
+        }
+
+        if config.ResolveIDs {
+                aucoalesce.ResolveIDs(auditEvent)
+        }
+
+}
+
+func receiveEvents(as *AuditSet, done <-chan struct{}) (<-chan []*auparse.AuditMessage, error) {
+
+    if err := initClient(as); err != nil {
+        return nil, err
+    }
+
+    out := make(chan []*auparse.AuditMessage, as.config.StreamBufferQueueSize)
+
+    return out, nil
+}
+
+func initClient(as *AuditSet) error {
+        if as.config.SocketType == "multicast" {
+                // This request will fail with EPERM if this process does not have
+                // CAP_AUDIT_CONTROL, but we will ignore the response. The user will be
+                // required to ensure that auditing is enabled if the process is only
+                // given CAP_AUDIT_READ.
+        }
+
+        // Unicast client initialization (requires CAP_AUDIT_CONTROL and that the
+        // process be in initial PID namespace).
+        status, err := as.client.GetStatus()
+        if err != nil {
+                return errors.Wrap(err, "failed to get audit status")
+        }
+        as.kernelLost.enabled = true
+        as.kernelLost.counter = status.Lost
+
+        glog.Infoln("audit status from kernel at start", "audit_status", status)
+
+        if status.Enabled == auditLocked {
+                return errors.New("failed to configure: The audit system is locked")
+        }
+
+        if fm, _ := as.config.failureMode(); status.Failure != fm {
+                if err = as.client.SetFailure(libaudit.FailureMode(fm), libaudit.NoWait); err != nil {
+                        return errors.Wrap(err, "failed to set audit failure mode in kernel")
+                }
+        }
+
+        if status.BacklogLimit != as.config.BacklogLimit {
+                if err = as.client.SetBacklogLimit(as.config.BacklogLimit, libaudit.NoWait); err != nil {
+                        return errors.Wrap(err, "failed to set audit backlog limit in kernel")
+                }
+        }
+
+        if as.backpressureStrategy&(bsKernel|bsAuto) != 0 {
+                // "kernel" backpressure mitigation strategy
+                //
+                // configure the kernel to drop audit events immediately if the
+                // backlog queue is full.
+                if status.FeatureBitmap&libaudit.AuditFeatureBitmapBacklogWaitTime != 0 {
+                        glog.Info("Setting kernel backlog wait time to prevent backpressure propagating to the kernel.")
+                        if err = as.client.SetBacklogWaitTime(0, libaudit.NoWait); err != nil {
+                                return errors.Wrap(err, "failed to set audit backlog wait time in kernel")
+                        }
+                } else {
+                        if as.backpressureStrategy == bsAuto {
+                                glog.Warning("setting backlog wait time is not supported in this kernel. Enabling workaround.")
+                                as.backpressureStrategy |= bsUserSpace
+                        } else {
+                                return errors.New("kernel backlog wait time not supported by kernel, but required by backpressure_strategy")
+                        }
+                }
+        }
+
+        if as.backpressureStrategy&(bsKernel|bsUserSpace) == bsUserSpace && as.config.RateLimit == 0 {
+                // force a rate limit if the user-space strategy will be used without
+                // corresponding backlog_wait_time setting in the kernel
+                as.config.RateLimit = 5000
+        }
+
+        if status.RateLimit != as.config.RateLimit {
+                if err = as.client.SetRateLimit(as.config.RateLimit, libaudit.NoWait); err != nil {
+                        return errors.Wrap(err, "failed to set audit rate limit in kernel")
+                }
+        }
+
+        if status.Enabled == 0 {
+                if err = as.client.SetEnabled(true, libaudit.NoWait); err != nil {
+                        return errors.Wrap(err, "failed to enable auditing in the kernel")
+                }
+        }
+        if err := as.client.WaitForPendingACKs(); err != nil {
+                return errors.Wrap(err, "failed to wait for ACKs")
+        }
+        if err := as.client.SetPID(libaudit.WaitForReply); err != nil {
+                if errno, ok := err.(syscall.Errno); ok && errno == syscall.EEXIST && status.PID != 0 {
+                        return fmt.Errorf("failed to set audit PID. An audit process is already running (PID %d)", status.PID)
+                }
+                return errors.Wrapf(err, "failed to set audit PID (current audit PID %d)", status.PID)
+        }
+        return nil
 }
 
 func newAuditClient(c *Config) (*libaudit.AuditClient, error) {
@@ -214,6 +386,19 @@ func determineSocketType(c *Config) (string, error) {
                 return unicast, nil
         }
 
+}
+
+func updateKernelLostMetric(as *AuditSet, lost uint32) {
+        if !as.kernelLost.enabled {
+                return
+        }
+        delta := int64(lost - as.kernelLost.counter)
+        if delta >= 0 {
+                glog.Warningf("kernel lost events: %d (total: %d)", delta, lost)
+        } else {
+                glog.Warningf("kernel lost event counter reset from %d to %d", as.kernelLost, lost)
+        }
+        as.kernelLost.counter = lost
 }
 
 func getBackpressureStrategy(value string) backpressureStrategy {
